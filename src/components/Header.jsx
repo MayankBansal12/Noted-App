b033db8fb634546051629a837a472e576c040343
Mayank <mayankbansal125@gmail.com>
2023-07-19 23:45:00 +0530

    Fix bug in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,10 @@ import NoteAltIcon from '@mui/icons-material/NoteAlt';
 function Header() {
   return (
     <header>
-      <div className="text">
         <h1>
           <NoteAltIcon />
           Noted
         </h1>
-        <p>Just Note it!</p>
-      </div>
       <button>Login</button>
     </header>
   );
